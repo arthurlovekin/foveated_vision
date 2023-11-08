725bef5da6b04ca1536a1b91bbe8aa80926dc109
Arthur Lovekin <62527557+arthurlovekin@users.noreply.github.com>
2023-11-07 19:22:38 -0500

    Interactive foveation

---
+++ src/foveation.py
@@ -0,0 +1,151 @@
+"""
+Methods for sampling points and creating a foveated image
+"""
+import numpy as np
+"""
+How do we want to represent the foveation?
+1. Set of sample points 
+    (Option 1) Low number (~16) of sample points
+         At each sample point, take an image sample (at some resolution) and assign it a position encoding
+    (Option 2) High number of sample points
+        At each sample point, take a max pool. (these are more pixelwise samples)
+2. CNN style 
+    Define the kernel size, step and stride, at several different scales
+
+    (Essentially a CNN that is parametrized such that the step and stride are 
+    variable and do not cover the entire image + positional encodings to keep things straight)
+"""
+# How do we want to represent the foveation?
+# 1. Set of sample points 
+#     1.1 At each sample point, take an image patch (at some resolution) and assign it a 
+
+
+#### Sampling Techniques ####
+def sample_geometric_sequence(center_x,center_y, nx, ny, width, height):
+    """
+    Input: 
+    Center pixel coordinates of the foveated region
+    nx, ny: number of points to sample in the +x and +y directions 
+    width: distance between leftmost and rightmost sampled points
+    height: distance between topmost and bottommost sampled points
+    Ouput: (2*nx+1 length np.array of x pixel coordinates, 2*ny+1 length np.array of y pixel coordinates)
+    """ 
+    x_points = np.zeros(2*nx+1)
+    y_points = np.zeros(2*ny+1)
+    x_points[-1] = center_x
+    y_points[-1] = center_y
+
+    # Generate geometric sequence of points
+    rx = 2
+    ry = 2
+    a0_x = width/2.0*(1-rx)/(1-rx**(nx))
+    a0_y = height/2.0*(1-ry)/(1-ry**(ny))
+    a_x = 0
+    a_y = 0
+    for i in range(nx):
+        a_x += a0_x*(rx**i)
+        x_points[i]=(center_x + a_x)
+        x_points[i+nx]=(center_x - a_x)
+    for i in range(ny):
+        a_y += a0_y*(ry**i)
+        y_points[i]=(center_y + a_y)
+        y_points[i+ny] =(center_y - a_y)
+
+    x_sorted_integers = np.sort(x_points.astype(int))
+    y_sorted_integers = np.sort(y_points.astype(int))
+    return x_sorted_integers, y_sorted_integers
+
+def sample_uniform(center_x,center_y, nx, ny, width, height):
+    """
+    Sample uniformly across the entire region (used for the base pooling)
+    """
+    x_points = np.linspace(center_x-width/2, center_x+width/2, nx)
+    y_points = np.linspace(center_y-height/2, center_y+height/2, ny)
+    return x_points.astype(int), y_points.astype(int)
+
+def sample_trapezoidal(center_x,center_y, nx, ny, width, height):
+    pass
+
+def sample_gaussian(center_x,center_y, nx, ny, width, height):
+    """
+    Deterministically Generate x and y points from 1D Gaussian distributions
+    the covariance is [[width, 0], [0, height]] 
+    """
+    pass
+    # uniform_points_x = np.linspace(0, 1, nx)
+    # uniform_points_y = np.linspace(0, 1, ny)
+
+    # # Apply the inverse cumulative distribution function (ICDF) of the normal distribution
+    # # This uses the percent point function (PPF) of the normal distribution
+    ## from scipy.special import erfinv
+
+    # # x_points = center_x + width * np.sqrt(2) * erfinv(2 * uniform_points_x - 1)
+    # # y_points = center_y + height * np.sqrt(2) * erfinv(2 * uniform_points_y - 1)
+    # x_sorted_ints = np.sort(x_points.astype(int))
+    # y_sorted_ints = np.sort(y_points.astype(int))
+    # return x_sorted_ints, y_sorted_ints
+
+def sample_polar(center_x,center_y, nx, ny, width, height):
+    pass
+
+#### Foveation Techniques ####
+def max_pool_image(img_array, factor):
+    """
+    Input: 
+    img_array: (width, height, 3) numpy array of the original image
+    factor: factor by which to downsample the image
+    Output:
+    numpy array of the downsampled image
+    """    
+    # Calculate the dimensions of the downsampled image
+    downsampled_width = img_array.shape[0] // factor
+    downsampled_height = img_array.shape[1] // factor
+    
+    # Reshape the image array and perform max pooling using strides
+    downsampled_array = img_array.reshape(downsampled_width, factor, downsampled_height, factor, 3).max(axis=(1, 3))
+
+    return downsampled_array
+
+
+def max_pool_foveation(points_x, points_y, img_array):
+    """
+    At each sample point, take a max pool of the surrounding pixels
+    (size of patch is proportional to distance from fovea center)
+    Input: 
+    points_x and points_y - ordered sample points from sample_geometric_sequence
+    img_array - (width, height, 3) numpy array of the foveated image (will be modified)
+    Output:
+    (width, height, 3) numpy.array of Foveated image
+    """
+    width = img_array.shape[0]
+    height = img_array.shape[1]
+
+    for i,px in enumerate(points_x):
+        for j,py in enumerate(points_y):
+            # patch radius is half the distance between the 
+            # current point and the next point towards the center (or 0.5)
+            patch_scale = 0.7
+            if i < len(points_x)/2:
+                patch_radius_x = int(abs(px-points_x[i+1])*patch_scale)
+            else:
+                patch_radius_x = int(abs(px-points_x[i-1])*patch_scale)
+            if j < len(points_y)/2:
+                patch_radius_y = int(abs(py-points_y[j+1])*patch_scale)
+            else:
+                patch_radius_y = int(abs(py-points_y[j-1])*patch_scale)
+
+            x_min = max(0, px-patch_radius_x)
+            y_min = max(0, py-patch_radius_y)
+            x_max = min(width, px+patch_radius_x)
+            y_max = min(height, py+patch_radius_y)
+            if x_min >= x_max or y_min >= y_max:
+                continue
+
+            max_pool = np.max(img_array[x_min:x_max, y_min:y_max, :], axis=(0,1))
+            img_array[x_min:x_max, y_min:y_max, :] = max_pool
+            
+    return img_array
+
+
+# CNN Foveation
+# FoveaTR foveation
---
+++ src/interactive_foveation.py
@@ -0,0 +1,73 @@
+"""
+pygame renderer for different foveation techniques
+"""
+
+import pygame
+import sys
+from foveation import *
+import numpy as np
+
+def main():
+    # Initialize Pygame
+    pygame.init()
+
+    # Set up display
+    screen_height = 600
+    screen_width = 600*2
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    pygame.display.set_caption("Foveated Vision Visualization")
+
+    # Load original_image 
+    original_image = pygame.image.load("abbeyroad.jpg")
+    original_image = pygame.transform.scale(original_image, (screen_width/2,screen_height))
+
+    clock = pygame.time.Clock()
+
+    # Event Loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    pygame.quit()
+                    sys.exit()
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        # Track the cursor
+        cursor_x, cursor_y = pygame.mouse.get_pos()
+
+        # Create the Foveated Image
+        nx = 30
+        ny = 30
+        fovea_width = 100
+        fovea_height = 100
+        points_x, points_y = sample_geometric_sequence(cursor_x, cursor_y, nx, ny, fovea_width, fovea_height)
+        # points_x, points_y = sample_gaussian(cursor_x, cursor_y, nx, ny, fovea_width, fovea_height)
+        all_points_x = points_x
+        all_points_y = points_y
+
+        # base_points_x, base_points_y = sample_uniform(screen_width/4, screen_height/2, nx, ny, screen_width/2, screen_height)
+        # all_points_x = np.sort(np.concatenate((base_points_x, points_x)))
+        # all_points_y = np.sort(np.concatenate((base_points_y, points_y)))
+        # for px in base_points_x:
+        #     for py in base_points_y:
+        #         pygame.draw.circle(screen, (0,0,255), (px,py), 3)
+
+        img_array = pygame.surfarray.array3d(original_image)
+        img_array = max_pool_foveation(all_points_x, all_points_y, img_array)
+        foveated_image = pygame.surfarray.make_surface(img_array)
+        
+        # Draw original_image onto the left, and foveated_image onto the right
+        screen.blit(original_image, (0, 0))
+        screen.blit(foveated_image, (screen_width/2, 0))
+
+        # Draw the sampled foveation points
+        for px in points_x:
+            for py in points_y:
+                pygame.draw.circle(screen, (255,0,0), (px,py), 3)
+        pygame.display.flip()
+        clock.tick(60)
+
+if __name__ == "__main__":
+    main()
