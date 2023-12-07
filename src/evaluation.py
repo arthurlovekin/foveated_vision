71133d270bcdecc18f4565b4217b16d2092f9e2e
arthurlovekin <arthurlovekin@gmail.com>
2023-12-07 09:48:25 -0500

    logging

--- src/evaluation.py
+++ src/evaluation.py
@@ -3,6 +3,7 @@ from got10k.experiments import ExperimentGOT10k
 import torch
 from peripheral_foveal_vision_model import PeripheralFovealVisionModel
 from torchvision.io import read_image
+import logging
 # pip3 install got10k
 # https://github.com/got-10k/toolkit
 class IdentityTracker(Tracker):
@@ -26,6 +27,7 @@ class FoveatedVisionTracker(Tracker):
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.model.to(self.device)
         self.model.eval()
+        logging.info(f"Loaded model. Device: {self.device} Filepath: {self.model_filepath}")
 
     def init(self, image, box):
         # TODO: should our model take in the starting bounding box as well?
@@ -37,6 +39,7 @@ class FoveatedVisionTracker(Tracker):
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
     # setup tracker
     # model_filepath = r'/home/alovekin/foveated_vision/models/20231206_185942_model_epoch_2_step_3760.pth'
     model_filepath_base = r'./models/'
@@ -44,6 +47,7 @@ if __name__ == '__main__':
     tracker = FoveatedVisionTracker(model_filepath)
 
     # run experiments on GOT-10k (validation subset)
+    logging.info('Running experiments on GOT-10k (validation subset)...')
     experiment = ExperimentGOT10k(
         root_dir=r'/scratch/engin_root/engin1/shared_data/group_raz/data/got10k',
         subset='val', #note that test ground-truth is withheld
